2492963b03b063f04f8221bee020923e910a42e6
Gai-S <sgaishab@gmail.com>
2024-01-09 01:40:58 +0200

    small style fixes

--- tasks_Jcode.js
+++ tasks_Jcode.js
@@ -74,7 +74,7 @@ function taskCompletion(){
     var completed_task=tasks[pressed_task-1]; //is it a cope or by reference?
     DisplayTask(completed_task,pressed_task);
     $("#popupBasic").popup("close");
-    $('ul').prepend(`<img id='heyyy' style='width: 20%; position:fixed; z-index: 1; min-width: 12em' src="${icons_arr[cur_icon]}">`);
+    $('ul').prepend(`<img id='heyyy' style='width: 20%; position:fixed; z-index: 2; min-width: 12em' src="${icons_arr[cur_icon]}">`);
     cur_icon=(cur_icon+1<icons_arr.length)?cur_icon+1:0;
     $("#heyyy").fadeIn(5500).animate({top: '10em',left: '10em', transform: 'rotate(20deg)'},500).fadeOut(500);
     pressed_task=-1;
